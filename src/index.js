0b012472cef8960ec813a05ae16a5c1f016de8eb
Cormac <cormac_browne@hotmail.com>
2025-03-29 17:27:28 +0000

    Updated upload flow and meme display

--- src/index.js
+++ src/index.js
@@ -1,11 +1,6 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
+import { supabase } from './supabaseClient';
+import App from './components/App';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+document.addEventListener('DOMContentLoaded', () => {
+  App();
+});
